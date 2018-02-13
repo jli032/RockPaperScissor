35f96c0624ec5ba97812affa72eb0595cdfab0c9
Jeffrey Li <jli032@ucr.edu>
2018-02-13 13:21:20 -0800

    Add win result functionality.

--- app/src/main/java/com/example/supremegoldieking/rockpaperscissor/MainActivity.java
+++ app/src/main/java/com/example/supremegoldieking/rockpaperscissor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.supremegoldieking.rockpaperscissor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                //If the player wins.
+                Intent winIntent = new Intent(getApplicationContext(), WinActivity.class);
+                startActivity(winIntent);
             }
         });
     }
